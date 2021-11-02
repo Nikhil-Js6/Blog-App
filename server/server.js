38f1a1b8a99f113480d509b61e8408a785ea2515
Nikhil <56617924+Nikhil-pahal-js@users.noreply.github.com>
2021-11-02 13:01:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require('mongoose');
-const app = express();
 const routes = require('./routes');
 const multer = require('multer');
 const path = require("path");
+
+const app = express();
 
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
