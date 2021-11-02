417ffcbc2edaf9cb70ef39d7e1fbbcbf0d1e6ade
Nikhil <56617924+Nikhil-pahal-js@users.noreply.github.com>
2021-11-02 12:56:30 +0530

    Create index.js

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import { ContextProvider } from './context/Context.js';
+
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>
+    ,document.getElementById('root')
+);
