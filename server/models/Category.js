aceee8cfdbc0ff1fa493e95d5e432aea9aa21b2a
Nikhil <56617924+Nikhil-pahal-js@users.noreply.github.com>
2021-11-18 22:30:24 +0530

    Add files via upload

---
+++ server/models/Category.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+     name: {
+        type: String,
+        required: true,
+     },
+},
+   { timestamps: true }
+);
+
+module.exports = new mongoose.model("Category", categorySchema, "Categories");
